25daeddf21afd232aa056821c45c9d351652c343
LalloLallo <edua031@gmail.com>
2023-06-14 19:36:16 -0300

    conexDB

--- models/Conex.php
+++ models/Conex.php
@@ -1,20 +1,38 @@
 <?php
 
+/*
+class Conexion {
+	public static function Conectar () {
+		define('servidor', 'localhost');
+		define('dbname', 'php_labdb');
+		define('usuario', 'root');
+		define('password', '');
+
+		$opciones = array (PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES latin1");
+
+		try {
+			$conexion = new PDO("mysql:host=".servidor."; dbname=".dbname, usuario, password, $opciones);
+			return $conexion;
+		} catch (Exception $e) {
+			die("El error de conexion es: ".$e->getMessage());
+		}
+	}
+}*/
 
 	$conex = mysqli_connect("localhost", "root", "", "php_labdb");
 	mysqli_set_charset($conex, "utf8");
 
-//function Conectarse() { 
-	/*$dbhost= "localhost"; 
+function Conectarse() { 
+	$dbhost= "localhost"; 
 	$dbuser= "root"; 
 	$dbpass= ""; 
-	$dbname= "practico3"; 
-	$link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); */
-	/*if (!$link){
+	$dbname= "php_labdb"; 
+	$link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	if (!$link){
 		echo 'Error en conexion a la base de datos';
 	} else {
 		return $link; 
-	}*/
-//} 
+	}
+} 
 
 ?>
